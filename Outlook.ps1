fa09e69be2d2e9d84c5077e86c31e69da8fc5a74
Wes Stahler <wes.stahler@gmail.com>
2017-09-07 09:17:59 -0400

    Added initial code for spam folder

--- Outlook.ps1
+++ Outlook.ps1
@@ -1,1 +1,13 @@
 # Exploring Outlook with PowerShell
+$outlook = New-Object -ComObject Outlook.Application
+$RootFolder = $outlook.Session.Folders | Where-Object FolderPath -eq '\\Spam Reporting (OSUWMC)'
+foreach ($folder in $RootFolder) {
+    foreach ($mailfolder in $folder.Folders) {
+        foreach ($subMailFolder in $mailfolder.Folders) {
+            $cnt = $submailfolder.UnReadItemCount
+            if ($cnt -gt 0) {
+                "{0}`t{1}" -f $submailfolder.UnReadItemCount, $submailfolder.FolderPath
+            }
+        }
+    }
+}

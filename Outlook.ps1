6e80713cebdb6546acb65a8c89336c899ca2a359
Wes Stahler <wes.stahler@gmail.com>
2017-09-07 10:57:50 -0400

    commit

--- Outlook.ps1
+++ Outlook.ps1
@@ -1,13 +1,14 @@
 # Exploring Outlook with PowerShell
 $outlook = New-Object -ComObject Outlook.Application
-$RootFolder = $outlook.Session.Folders | Where-Object FolderPath -eq '\\Spam Reporting (OSUWMC)'
+$RootFolder = $outlook.Session.Folders.Item("Spam Reporting (OSUWMC)").Folders
+
 foreach ($folder in $RootFolder) {
     foreach ($mailfolder in $folder.Folders) {
-        foreach ($subMailFolder in $mailfolder.Folders) {
-            $cnt = $submailfolder.UnReadItemCount
-            if ($cnt -gt 0) {
-                "{0}`t{1}" -f $submailfolder.UnReadItemCount, $submailfolder.FolderPath
-            }
-        }
-    }
+        foreach ($subMailFolder in $mailfolder.Folders) {
+            $cnt = $submailfolder.UnReadItemCount
+            if ($cnt -gt 0) {
+                "{0}`t{1}" -f $submailfolder.UnReadItemCount, $submailfolder.FolderPath
+            }
+        }
+    }
 }
---
+++ Outlook2.ps1
@@ -0,0 +1,37 @@
+﻿function Get-MailboxFolder {
+    param($folder)
+
+    [pscustomobject]@{
+        Path = $folder.FullFolderPath
+        Count = $folder.items.count
+    }
+
+    foreach ($f in $folder.folders) {
+        Get-MailboxFolder $f
+    }
+}
+
+$obj = @()
+
+$outlook = New-Object -ComObject Outlook.Application
+$RootFolder = $outlook.Session.Folders.Item("Spam Reporting (OSUWMC)").Folders
+foreach ($folder in $RootFolder) {
+    foreach ($mailfolder in $folder.Folders) {
+        $obj+=Get-MailboxFolder $mailfolder
+    }
+}
+
+# format the date to match the Outlook folder format
+$dt = Get-Date (Get-Date).AddDays(-1) -Format "MM-dd-yyyy"
+
+# define our list of folder that we want numbers from
+$folders = '\\Spam Reporting (OSUWMC)\Inbox\Non Report SPAM\',
+           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\By Date\',
+           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\eusafe\',
+           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\safe\'
+
+foreach ($folder in $folders) {
+    $path = "$folder$dt"
+    $obj.where({$_.path -eq $path})
+}
+

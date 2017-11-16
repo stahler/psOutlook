d24826f8d12a45932c1d353c7b855f4eee95cb8f
Wes Stahler <wes.stahler@gmail.com>
2017-11-16 09:51:42 -0500

    Refactored to add parameters

--- Get-OutlookFolderInfo.ps1
+++ Get-OutlookFolderInfo.ps1
@@ -11,27 +11,48 @@
     }
 }
 
-$obj = @()
+function Get-OutlookFolderCount {
+    [cmdletbinding()]
+    param(
+        $root = "Spam Reporting (OSUWMC)",
+        $folders = $null
+    )
 
-$outlook = New-Object -ComObject Outlook.Application
-$RootFolder = $outlook.Session.Folders.Item("Spam Reporting (OSUWMC)").Folders
-foreach ($folder in $RootFolder) {
-    foreach ($mailfolder in $folder.Folders) {
-        $obj+=Get-MailboxFolder $mailfolder
+    process {
+        $obj = @()
+        try {
+            $outlook = New-Object -ComObject Outlook.Application -ErrorAction Stop
+            $RootFolder = $outlook.Session.Folders.Item($root).Folders
+        }
+        catch [System.Runtime.InteropServices.COMException] {
+            Write-Warning "Outlook not installed or root folder does not exist"
+            break
+        }
+        # iterates over all folders and ultimatly gets the full path and count
+        foreach ($folder in $RootFolder) {
+            foreach ($mailfolder in $folder.Folders) {
+                $obj+=Get-MailboxFolder $mailfolder
+            }
+        }
+        
+        if ($folders) {
+            $dt = Get-Date (Get-Date).AddDays(-1) -Format "MM-dd-yyyy"
+            foreach ($folder in $folders) {
+                $path = "$folder$dt"
+                $obj.where({$_.path -eq $path})
+            }
+        } else {
+            $obj
+        }
     }
 }
 
-# format the date to match the Outlook folder format
-$dt = Get-Date (Get-Date).AddDays(-1) -Format "MM-dd-yyyy"
-
 # define our list of folder that we want numbers from
 $folders = '\\Spam Reporting (OSUWMC)\Inbox\Non Report SPAM\',
-           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\By Date\',
-           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\eusafe\',
-           '\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\safe\'
+'\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\By Date\',
+'\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\eusafe\',
+'\\Spam Reporting (OSUWMC)\Inbox\Report Phish Reports\safe\'
 
-foreach ($folder in $folders) {
-    $path = "$folder$dt"
-    $obj.where({$_.path -eq $path})
-}
-
+#Get-OutlookFolderCount -root "Spam Reporting (OSUWMC)" | Out-GridView
+#Get-OutlookFolderCount -root "Spam Reporting (OSUWMC))" -folders $folders | Out-GridView
+#Get-OutlookFolderCount -root "Wes.Stahler@osumc.edu" | Out-GridView
